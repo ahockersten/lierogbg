37f47183858a72ad026f0678c9d68f32356a819b
Anders Höckersten <anders@hockersten.se>
2014-09-01 14:57:34 +0200

    Display colored worm next to name
    Worms, worms everywhere!

--- js/lierogbg.js
+++ js/lierogbg.js
@@ -77,4 +77,45 @@ $(document).ready(function () {
     $(".delete_subgame_form").click(function () {
         return deleteSubgameForm(this, "subgame_set");
     });
+
+    $('img.svg').each(function(){
+        var img = $(this);
+        var imgID = img.attr('id');
+        var imgClass = img.attr('class');
+        var imgURL = img.attr('src');
+
+        $.get(imgURL, function(data) {
+            // Get the SVG tag, ignore the rest
+            var svg = $(data).find('svg');
+
+            // Add replaced image's ID to the new SVG
+            if (typeof imgID !== 'undefined') {
+                svg = svg.attr('id', imgID);
+            }
+            // Add replaced image's classes to the new SVG
+            if (typeof imgClass !== 'undefined') {
+                svg = svg.attr('class', imgClass + ' replaced-svg');
+            }
+
+            // Remove any invalid XML tags as per http://validator.w3.org
+            svg = svg.removeAttr('xmlns:a');
+
+            // Replace image with new SVG
+            img.replaceWith(svg);
+
+            // FIXME this is inefficient. we need only do it for the last svg loaded
+            $(".lieroworm").each(function(index) {
+                var color = $(this).attr('id');
+                $(this).find("#svg_light_color").each(function(index) {
+                    $(this).css({'fill' : shadeColor(color, -0.2)});
+                });
+                $(this).find("#svg_medium_color").each(function(index) {
+                    $(this).css({'fill' : color});
+                });
+                $(this).find("#svg_dark_color").each(function(index) {
+                    $(this).css({'fill' : shadeColor(color, 0.2)});
+                });
+            });
+        }, 'xml');
+    });
 });
