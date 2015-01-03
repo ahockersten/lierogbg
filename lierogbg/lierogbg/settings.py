f63fe150b0d5fd9469f21ada7032eb8ad32fed55
Anders Höckersten <anders@hockersten.se>
2015-01-03 14:36:18 +0100

    Reenable debug for debug builds

--- lierogbg/lierogbg/settings.py
+++ lierogbg/lierogbg/settings.py
@@ -7,7 +7,7 @@ from django.conf.global_settings import TEMPLATE_CONTEXT_PROCESSORS as TCP
 
 here = lambda *x: os.path.join(os.path.abspath(os.path.dirname(__file__)), *x)
 
-DEBUG = False
+DEBUG = True
 TEMPLATE_DEBUG = DEBUG
 
 ADMINS = (
