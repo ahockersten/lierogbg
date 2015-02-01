eeec6686c225059b4781f3a76ca368942d893420
Anders Höckersten <anders@hockersten.se>
2015-02-01 13:00:06 +0100

    Corrected media location

--- lierogbg/lierogbg/settings.py
+++ lierogbg/lierogbg/settings.py
@@ -12,7 +12,7 @@ DEBUG = True
 TEMPLATE_DEBUG = DEBUG
 
 ADMINS = (
-    ('Anders Hockersten', 'anders@maskinskrift.com'),
+    ('Anders Hockersten', 'chucky@orbmit.org'),
 )
 
 MANAGERS = ADMINS
@@ -64,7 +64,7 @@ MEDIA_ROOT = '/var/www/lierogbg/media'
 # URL that handles the media served from MEDIA_ROOT. Make sure to use a
 # trailing slash.
 # Examples: "http://media.lawrence.com/media/", "http://example.com/media/"
-MEDIA_URL = 'http://lierogbg.maskinskrift.com/media/'
+MEDIA_URL = 'http://lierogbg.orbmit.org/media/'
 
 # Absolute path to the directory static files should be collected to.
 # Don't put anything in this directory yourself; store your static files
