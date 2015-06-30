466277523dd72ce3b519c367177f46a0f16735f2
Anders Höckersten <anders@hockersten.se>
2015-06-30 08:36:38 +0200

    Use lierogbg-env not web-env

--- update.sh
+++ update.sh
@@ -5,6 +5,6 @@ export PYENV_ROOT="`pwd`/pyenv"
 export PATH="$PYENV_ROOT/bin:$PATH"
 eval "$(pyenv init -)"
 eval "$(pyenv virtualenv-init -)"
-pyenv activate web-env
+pyenv activate lierogbg-env
 pip install -r requirements.txt
 bower install
